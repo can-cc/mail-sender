85b8297b39dc128cc6a16d89dfb338bd7218e6d8
fwchen <me@chenfangwei.com>
2018-11-23 17:34:03 +0800

    read command line flag

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"flag"
 	"github.com/fwchen/mail-sender/email"
 	"github.com/fwchen/mail-sender/email/mailgun"
 	"github.com/fwchen/mail-sender/web"
 )
 
 func main() {
-	client := web.NewHTTPClient()
-	sender := mailgun.NewSender(client, "xxx", "0cc9d726")
-	to := email.Recipient{
-		Name:    "Me",
-		Address: "xxx",
+	mailgunApi := flag.String("mailgun-api", "", "mailgun api")
+	domain := flag.String("domain", "", "mailgun domain")
+	noReply := flag.String("noreply", "", "mailgun noreply")
+	recipient := flag.String("recipient", "", "recipient address")
+	recipientName := flag.String("target-name", "", "recipient name")
+	flag.Parse()
+
+	if len(*mailgunApi) <= 0 {
+		panic("mailgun-api is required.")
 	}
-	sender.Send("xxx@xx", email.Params{
-		"name": "Hello",
-	}, "Test", to)
+	if len(*domain) <= 0 {
+		panic("domain is required.")
+	}
+	if len(*noReply) <= 0 {
+		panic("noreply is required.")
+	}
+	if len(*recipient) <= 0 {
+		panic("target is required.")
+	}
+
+	client := web.NewHTTPClient()
+	sender := mailgun.NewSender(client, *domain, *mailgunApi)
+	to := email.Recipient{
+		Name:    *recipientName,
+		Address: *recipient,
+	}
+	sender.Send(*noReply, email.Params{}, "Test", to)
 }
